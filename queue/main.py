7a28058d7f70da1912a045860b3b0fe25c80ad89
data-detectiv <oppongyaw049@gmail.com>
2025-05-30 14:45:25 -0400

    circular queue implemented

--- queue/main.py
+++ queue/main.py
@@ -14,6 +14,7 @@ element = data.pop(0)
 - isFull - check if the queue is full
 - peek - get the value from the front of the queue without removing it
 """
+# Simple Queue 
 class Queue:
     def __init__(self, k):
         self.k = k
@@ -51,16 +52,76 @@ class Queue:
             for i in range(self.head, self.tail + 1):
                 print(self.queue[i], end=" ")
             print()
-            
-queue = Queue(5)
-queue.enqueue(1)
-queue.enqueue(2)
-queue.enqueue(3)
-queue.enqueue(4)
-queue.enqueue(5)
-print("Initial queue")
-queue.printQueue()
 
-queue.dequeue()
+class MyCircularQueue:
+    def __init__(self, k):
+        self.k = k
+        self.queue = [None] * k
+        self.head = self.tail = -1
+
+    def enqueue(self, data):
+        if ((self.tail + 1) % self.k == self.head):
+            print("The circular queue is full\n")
+        elif (self.head == -1):
+            self.head = 0
+            self.tail = 0
+            self.queue[self.tail] = data
+        else:
+            self.tail = (self.tail + 1) % self.k
+            self.queue[self.tail] = data
+
+    def dequeue(self):
+        if (self.head == -1):
+            print("The queue is empty\n")
+        elif (self.head == self.tail):
+            temp = self.queue[self.head]
+            self.head = -1
+            self.tail = -1
+            return temp
+        else:
+            temp = self.queue[self.head]
+            self.head = (self.head + 1) % self.k
+            return temp
+    def printCQueue(self):
+        if (self.head == -1):
+            print("No element in the circular queue")
+        elif (self.tail >= self.head):
+            for i in range(self.head, self.tail + 1):
+                print(self.queue[i], end=" ")
+            print()
+        else:
+            for i in range(self.head, self.k):
+                print(self.queue[i], end=" ")
+            for i in range(0, self.tail + 1):
+                print(self.queue[i], end=" ")
+            print()
+
+
+# Your MyCircularQueue object will be instantiated and called as such:
+obj = MyCircularQueue(5)
+obj.enqueue(1)
+obj.enqueue(2)
+obj.enqueue(3)
+obj.enqueue(4)
+obj.enqueue(5)
+print("Initial queue")
+obj.printCQueue()
+
+obj.dequeue()
 print("After removing an element from the queue")
-queue.printQueue()
+obj.printCQueue()
+obj.enqueue(6)
+obj.printCQueue()
+
+# queue = Queue(5)
+# queue.enqueue(1)
+# queue.enqueue(2)
+# queue.enqueue(3)
+# queue.enqueue(4)
+# queue.enqueue(5)
+# print("Initial queue")
+# queue.printQueue()
+
+# queue.dequeue()
+# print("After removing an element from the queue")
+# queue.printQueue()
